fb2846dcf391e31d8f0032db0a8ec0ba235da9ec
Gilad Goldberg <giladgo@gmail.com>
2015-06-03 19:49:38 +0300

    Fix deploy

--- config.rb
+++ config.rb
@@ -56,10 +56,10 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
@@ -68,7 +68,9 @@ configure :build do
   # activate :relative_assets
 
   # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  set :http_prefix, "/bestknownfor/"
+
+
 end
 
 activate :deploy do |deploy|
@@ -79,3 +81,4 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
